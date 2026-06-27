4f868f401286edaa9d4d0f4ecc711d8e61fc480a
Joao Flavio <joaoflaviolima53@gmail.com>
2026-06-27 17:26:51 -0300

    Classe Processos foi adaptada para receber entrada da Fabrica De Operações. O construtor foi corrigido e foi criada a função executarA para melhorar a leitura, pois seu conteudo estava previamente dentro do metodo run.

--- ProjetoSO/src/main/java/br/upe/Processos.java
+++ ProjetoSO/src/main/java/br/upe/Processos.java
@@ -1,61 +1,53 @@
 package br.upe;
 
 public class Processos implements Runnable {
-
-	private String entrada;
-	private static int idThread = 0;
-	private String[] operacoes; 
-	// pelo que entendi do diagrama, virá de uma classe separada da main
 	
+	private String[] operacoes; // Todas as operações separadamente.
+	private static int contadorId = 0;
+	private int idThread; // id individual do processo.
 	private static GerenciaMemoria gerenciador;
 	
-	
-	public Processos(GerenciaMemoria gerenciador) {
-		this.entrada = entrada;
-		this.idThread = idThread;
-		idThread++;
-		this.operacoes = entrada.split(",");
+	public Processos(String entrada) {
+		this.idThread = contadorId++;
+		this.operacoes = entrada.split(","); 
+		// Primeira divisão: operações juntas para individuais.
 	}
 	
 	@Override
 	public void run() {
 		System.out.println("processo" + idThread + "iniciado com sucesso. :)");
 		
-		//Loop para passar pelo array de operações.
-		for(String cadaOperacao: operacoes) {
-			try {
-				// .slip-operacoes para inicializar as variaveis.
-				// estou supondo que elas vem na forma "operação-endereço-valor"
-				// e separados por virgula.
-				String[] corte = cadaOperacao.split(",");
-				char operacao = corte[0].charAt(0);
-				// essa linha acima ainda me deixa confuso.
-				int index = Integer.parseInt(corte[1]);
-				int valor = Integer.parseInt(corte[2]);
-				
-				// um if para separar as operações.
-				if (operacao == 'L') {
-					Leitura(index, valor);
-				} else if (operacao == 'E') {
-					Escrita(index, valor);
-				}
-				
-				
-			} catch (Exception e) {
-				System.err.println("Erro no processo numero " + idThread);
+		try {
+			//Loop para passar pelo array de operações.
+			for(String cadaOperacao: operacoes) {
+				executarA(cadaOperacao);
 			}
+		} catch (Exception e) {
+			System.err.println("Erro no processo numero " + idThread);
 		}
+	}
 		
+	private void executarA(String operacaoDaVez) {
+		// um if separa leitura e escrita e chama a função adequada.
+		if (operacaoDaVez.contains("-W-")) {
+			String[] argumentos = operacaoDaVez.split("-W-");
+			int index = Integer.parseInt(argumentos[0]);
+			int valor = Integer.parseInt(argumentos[1]);
+			Escrita(index, valor);
+		} else if (operacaoDaVez.contains("-R")) {
+			int index = Integer.parseInt(operacaoDaVez);
+			Leitura(index);
+		}
 	}
 	
-	// precisam de um idex da memoria e um valor para receber/enviar
 	public void Escrita(int index, int valor) {
-		gerenciador.acessarEndereco(index, 'E', valor);
+		gerenciador.acessarEndereco(index, 'w', valor);
 		System.out.println("Processo" + idThread + ": Escreveu " + valor + ": em index" + index + ".'");
 	}
 	
-	public void Leitura(int index, int valor) {
-		gerenciador.acessarEndereco(index, 'L', valor);
+	public void Leitura(int index) {
+		// Estou assumindo que o gerenciador retorna o valor lido.
+		valor = gerenciador.acessarEndereco(index, 'r');
 		System.out.println("Processo" + idThread + ": Leu " + valor + ": do index" + index + ".'");
 	}
 }
